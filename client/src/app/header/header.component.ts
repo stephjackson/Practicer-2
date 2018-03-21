68ce87112477a1e49be297b79cbbde534ee7da69
stephjackson <steph.garrett.jackson@gmail.com>
2018-03-21 10:03:30 -0400

    Styled navbar

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -2,17 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    template: `
-        <header class="row">
-            <nav class="col-md-8 col-md-offset-2">
-                <ul class="nav nav-pills">
-                    <li routerLinkActive="active"><a [routerLink]="['/lists']">Lists</a></li>
-                    <li routerLinkActive="active"><a [routerLink]="['/items']">List Items</a></li>
-                    <li routerLinkActive="active"><a [routerLink]="['/auth']">Authentication</a></li>
-                </ul>
-            </nav>
-        </header>
-    `
+    templateUrl: 'header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
